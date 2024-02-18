c342e9969c87dea2ffef4d1a4509116a852d8354
Diana Koralski <dianakoralski@hotmail.com>
2024-02-17 20:38:50 -0800

    update stable

--- lwp.c
+++ lwp.c
@@ -15,9 +15,9 @@
 #define STACK_SIZE (1024*1024*8) // 8 MB
 #define MAX_LWPS 32
 
-tid_t tid_counter = 1;
+tid_t last_tid = 0;
 thread current = NULL;
-scheduler this_scheduler = NULL;
+scheduler current_scheduler = NULL;
 thread* threads[MAX_LWPS]; // Mapping tid to thread
 
 typedef struct general_list{
@@ -28,112 +28,112 @@ typedef struct general_list{
 
 void w_admit(thread new, list *this_list);
 void w_remove(thread victim, list *this_list);
-void lwp_wrap(lwpfun fun, void *arg);
 
 list wait_list = {w_admit, w_remove, NULL};
 list blocked_list = {w_admit, w_remove, NULL};
 
 struct scheduler rr_publish = {rr_init, rr_shutdown, rr_admit, rr_remove, rr_next, rr_qlen};
-scheduler RoundRobin = &rr_publish;
+scheduler rr_scheduler = &rr_publish;
 
 void lwp_set_scheduler(scheduler x){
-	this_scheduler = x;
-	if (this_scheduler->init){
-		this_scheduler->init();
+	current_scheduler = x;
+	if (current_scheduler->init){
+		current_scheduler->init();
 	}
 }
 
 scheduler lwp_get_scheduler(void){
-	return this_scheduler;	
+	return current_scheduler;	
 }
 
 void w_admit(thread new, list *this_list){
 	if (new == NULL){
-        	return;
-    	}
-    	if (this_list->head == NULL){
-       		this_list->head = new;
+		return;
+	}
+	if (this_list->head == NULL){
+		this_list->head = new;
 		new->sched_one = NULL;
-      	}
-      	else{
-          	thread current_thread = this_list->head;
-          	while (current_thread->sched_one != NULL){
-                   	current_thread = current_thread->sched_one;
-           	}
-             	current_thread->sched_one = new;
-        	new->sched_one = NULL;                                         
+	}
+	else{
+		thread current_thread = this_list->head;
+		while (current_thread->sched_one != NULL){
+			current_thread = current_thread->sched_one;
+		}
+		current_thread->sched_one = new;
+		new->sched_one = NULL;                                         
 	}
 }
 
 void w_remove(thread victim, list *this_list){
-    if (victim == NULL){
+    if (victim == NULL || this_list->head == NULL){
         return;
     }
     thread previous_thread = NULL;
     thread current_thread = this_list->head;
-    if (current_thread == NULL){
-        return;
-    }
-    else {
-        while (current_thread != NULL){
-            if (current_thread->tid == victim->tid){
-                if(previous_thread == NULL){
-                    this_list->head = current_thread->sched_one;
-                }
-                else{
-                    previous_thread->sched_one = current_thread->sched_one;
-                }
-                return;
-            }
-            previous_thread = current_thread;
-            current_thread = current_thread->sched_one;
-        }
-    }
+	while (current_thread != NULL){
+		if (current_thread->tid == victim->tid){
+			if(previous_thread == NULL){
+				this_list->head = current_thread->sched_one;
+			}
+			else{
+				previous_thread->sched_one = current_thread->sched_one;
+			}
+			break;
+		}
+		previous_thread = current_thread;
+		current_thread = current_thread->sched_one;
+	}
+    
+}
+
+void lwp_wrap(lwpfun fun, void *arg) {
+	int rval = fun(arg);
+	lwp_exit(rval);
 }
 
 tid_t lwp_create(lwpfun function,void *argument){
-   	if (this_scheduler == NULL){
-		lwp_set_scheduler(RoundRobin);
+   	if (current_scheduler == NULL){
+		lwp_set_scheduler(rr_scheduler);
 	}
 	
 	thread new_thread = (thread)malloc(sizeof(context));
     unsigned long *base = (unsigned long *)mmap(NULL, STACK_SIZE,
-	PROT_READ|PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS|MAP_STACK,-1,0);
+		PROT_READ|PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS|MAP_STACK,-1,0);
     if (base == MAP_FAILED){
         return NO_THREAD;
     }
 	new_thread->stacksize = STACK_SIZE;
-    unsigned long *TOS = base + (new_thread->stacksize/sizeof(unsigned long));
+    unsigned long *offset = base + (new_thread->stacksize/sizeof(unsigned long));
     new_thread->stack = base;
-    TOS--;
-    *TOS = (unsigned long )lwp_wrap;
-    TOS--;
-    new_thread->tid = tid_counter;
-    tid_counter++;
-    new_thread->state.rsp = (unsigned long) TOS;
-    new_thread->state.rbp = (unsigned long) TOS;
+    offset--;
+    *offset = (unsigned long )lwp_wrap;
+    offset--;
+	last_tid++;
+    new_thread->tid = last_tid;
+    new_thread->state.rsp = (unsigned long) offset;
+    new_thread->state.rbp = (unsigned long) offset;
     new_thread->state.fxsave=FPU_INIT;
     new_thread->state.rdi = (unsigned long) function;
     new_thread->state.rsi = (unsigned long) argument;
 	new_thread->lib_one = NULL;
 	new_thread->status = LWP_LIVE;
-    this_scheduler->admit(new_thread);
+    current_scheduler->admit(new_thread);
 	threads[new_thread->tid] = &new_thread;
 	return new_thread->tid;
 }
 
 
 void lwp_start(void){
-	if (this_scheduler == NULL){
-        lwp_set_scheduler(RoundRobin);
+	if (current_scheduler == NULL){
+        lwp_set_scheduler(rr_scheduler);
     }
-	thread new_thread = (thread)malloc(sizeof(context));
-	new_thread->stack = NULL;
-	new_thread->tid = 0;
-	new_thread->lib_one = NULL;
-	new_thread->status = LWP_LIVE;
-	this_scheduler->admit(new_thread);
-	current = new_thread;
+	thread main_thread = (thread)malloc(sizeof(context));
+	main_thread->stack = NULL;
+	main_thread->tid = 0;
+	main_thread->lib_one = NULL;
+	main_thread->status = LWP_LIVE;
+	current_scheduler->admit(main_thread);
+	current = main_thread;
 	lwp_yield();
 }
 
@@ -142,8 +142,8 @@ void lwp_exit(int status){
 	current->status = new_status;
 	if(current->tid == 0){
 		free(current);
-		if (this_scheduler->shutdown){
-			this_scheduler->shutdown();
+		if (current_scheduler->shutdown){
+			current_scheduler->shutdown();
 		}
 		return;		
 	}
@@ -156,7 +156,7 @@ tid_t lwp_wait(int *status){
 		if (rr_qlen() == 0){
 		 	return NO_THREAD;
 		}
-		this_scheduler->remove(current);
+		current_scheduler->remove(current);
 		blocked_list.admit(current, &blocked_list);
 		lwp_yield();
 	}
@@ -164,58 +164,53 @@ tid_t lwp_wait(int *status){
 		current->lib_one = wait_list.head;
 		wait_list.remove(wait_list.head, &wait_list);
 	}
-	if(current->lib_one != NULL){
-		thread dying_thread = current->lib_one;
-		tid_t return_val = dying_thread->tid;
-		if(status != NULL){
-			*status = dying_thread->status;
-		}
-		if(munmap(dying_thread->stack, dying_thread->stacksize) == -1){
-			printf("munmap failed\n");
-		}
-		free(dying_thread);
-		current->lib_one = NULL;
-		return return_val;		
-	}
-	else{
-		printf("waiting thread does not have lib_one field\n");
+	thread terminated = current->lib_one;
+	if(terminated == NULL){
 		return NO_THREAD;
-	}	
-		
+	}
+	if(status != NULL){
+		*status = terminated->status;
+	}
+	if(munmap(terminated->stack, terminated->stacksize) == -1){
+		printf("munmap error\n");
+	}
+	tid_t return_val = terminated->tid;
+	free(terminated);
+	current->lib_one = NULL;
+	return return_val;
 }
 
 void lwp_yield(void){
-	thread old = current;
-	if(LWPTERMINATED(old->status)){
-		this_scheduler->remove(old);
+	thread previous = current;
+	if(LWPTERMINATED(previous->status)){
+		//when the thread is terminated we need to put it in a blocked_list or wait_list
+		current_scheduler->remove(previous);
 		thread current_blocked = blocked_list.head;
 		int waitlist_flag = 1;
-		if(current_blocked != NULL){
 		while (current_blocked != NULL){
 			if(current_blocked->lib_one == NULL){
-				current_blocked->lib_one = old;
+				current_blocked->lib_one = previous;
 				blocked_list.remove(current_blocked, 
 				&blocked_list);
-				this_scheduler->admit(current_blocked);
+				current_scheduler->admit(current_blocked);
 				waitlist_flag = 0;
 				break;
 			}
 			current_blocked = current_blocked->sched_one;
 		}
-		}
 		if(waitlist_flag){
-			wait_list.admit(old,&wait_list);	
+			wait_list.admit(previous,&wait_list);	
 		}						
 	}
-	current = this_scheduler->next();
-	if(current == old){
+	current = current_scheduler->next();
+	if(previous == current){ //the last thread is the main thread
 		return;
 	}
-	swap_rfiles(&old->state, &current->state);
+	swap_rfiles(&previous->state, &current->state);
 }
 
 tid_t lwp_gettid(){
-	return current->tid;
+	return current == NULL ? 0 : current->tid;
 }
 
 thread tid2thread(tid_t tid){
@@ -224,10 +219,3 @@ thread tid2thread(tid_t tid){
 	}
 	return NULL;	
 }
-
-void lwp_wrap(lwpfun fun, void *arg) {
-	int rval;
-	rval=fun(arg);
-	lwp_exit(rval);
-}
-
