9d5e33b5768d8c99dbe602ea8b931131082e49c8
Diana Koralski <dianakoralski@hotmail.com>
2024-02-17 21:07:54 -0800

    stable update again

--- rr.c
+++ rr.c
@@ -18,70 +18,65 @@ void rr_shutdown(void){
     qlen = 0;
 }
 
-
 void rr_admit(thread new){
     if (new == NULL){
         return;
     }
     if (head == NULL){
         head = new;
+        tail = new;
         new->sched_one = NULL;
     }
     else{
         thread current_thread = head;
-        while (current_thread->sched_one != NULL){
-            current_thread = current_thread->sched_one;
-        }
-        current_thread->sched_one = new;
+        tail->sched_one = new;
         new->sched_one = NULL;
+        tail = new;
     }
 	qlen++;
 }
 
 void rr_remove(thread victim){
-    if (victim == NULL){
+    if (victim == NULL || head == NULL){
         return;
     }
     thread previous_thread = NULL;
     thread current_thread = head;
-    if (current_thread == NULL){
-        return;
-    }
-    else {
-        while (current_thread != NULL){
-            if (current_thread->tid == victim->tid){
-                if(previous_thread == NULL){
-                    head = current_thread->sched_one;
+    while (current_thread != NULL){
+        if (current_thread->tid == victim->tid){
+            if(previous_thread == NULL){
+                head = current_thread->sched_one;
+                if (head == NULL){
+                    tail = NULL;
                 }
-                else{
-                    previous_thread->sched_one = current_thread->sched_one;
-                }
-				qlen--;
-                return;
             }
-            previous_thread = current_thread;
-            current_thread = current_thread->sched_one;
+            else{
+                previous_thread->sched_one = current_thread->sched_one;
+                if (tail == current_thread){
+                    tail = previous_thread;
+                }
+            }
+            qlen--;
+            break;
         }
+        previous_thread = current_thread;
+        current_thread = current_thread->sched_one;
     }
 }
 
 thread rr_next(void){
-        if(head == NULL){
-                return NULL;
+        if(head == NULL){ // no elements
+            return NULL;
         }
-        if(head->sched_one == NULL){
-                return head;
+        if(head->sched_one == NULL){ // only 1 element
+            return head;
         }
-        thread current_thread = head;
-        while (current_thread->sched_one != NULL){
-                current_thread = current_thread->sched_one;
-        }
-        thread next = head;
+        thread new_tail = head;
         head = head->sched_one;
-        current_thread->sched_one = next;
-        next->sched_one = NULL;
-        //printf("new head = %d, new tail = %d\n", head->tid, next->tid);
-     	return next;
+        tail->sched_one = new_tail;
+        new_tail->sched_one = NULL;
+        tail = new_tail;
+     	return tail;
 }
         
 int rr_qlen(){
